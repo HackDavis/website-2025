fc65d12ed5e72503dde1bdf4d52ef2d6b9fc459c
Win Cheng <winzamark12@gmail.com>
2024-11-15 16:04:56 -0800

    Added font to tailwind and globals

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
-      <p>Halo! Welcome to the HackDavis template repo :D</p>
+      <p className="text-4xl font-bold font-metropolis">Halo! Welcome to the HackDavis template repo :D</p>
+      <p className="text-4xl font-bold font-montserrat">Halo! Welcome to the HackDavis template repo :D</p>
+      <p className="text-4xl font-bold font-inter">Halo! Welcome to the HackDavis template repo :D</p>
     </div>
   );
 }
