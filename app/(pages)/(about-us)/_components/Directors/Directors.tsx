c4774f5b2abda43c8856b9fd212157c4151b55b3
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-11-15 17:01:47 -0800

    Created files for Director component

---
+++ app/(pages)/(about-us)/_components/Directors/Directors.tsx
@@ -0,0 +1,9 @@
+import styles from './Directors.module.scss';
+
+export default function Directors() {
+  return (
+    <div className={styles.container}>
+        Directors section
+    </div>
+  );
+}
--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,10 @@
+import Directors from "./(about-us)/_components/Directors/Directors";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
       <p>Halo! Welcome to the HackDavis template repo :D</p>
+      <Directors/>
     </div>
   );
 }
