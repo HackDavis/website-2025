fb80fcdc3ee6a183b3e8e4ceab96288e3696ede3
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-11-19 13:27:44 -0800

    Added buttons for team navigation

--- app/(pages)/(about-us)/_components/Directors/Directors.tsx
+++ app/(pages)/(about-us)/_components/Directors/Directors.tsx
@@ -3,7 +3,19 @@ import styles from './Directors.module.scss';
 export default function Directors() {
   return (
     <div className={styles.container}>
-        Directors section
+
+        <div className={styles.text}>
+          <h1>Meet our Directors</h1>
+          <h3>HackDavis is built by students, for students.</h3>
+        </div>
+
+        <div className={styles.buttonsContainer}>
+          <div className={styles.buttons}>Team 1</div>
+          <div className={styles.buttons}>Team 1</div>
+          <div className={styles.buttons}>Team 1</div>
+          <div className={styles.buttons}>Team 1</div>
+        </div>
+
     </div>
   );
 }
