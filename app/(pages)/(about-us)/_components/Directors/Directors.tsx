aa063421caed9a8ebba8ad00bd94aea08c4baf9f
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-11-19 17:23:32 -0800

    Added individual profile styling

--- app/(pages)/(about-us)/_components/Directors/Directors.tsx
+++ app/(pages)/(about-us)/_components/Directors/Directors.tsx
@@ -10,10 +10,31 @@ export default function Directors() {
         </div>
 
         <div className={styles.buttonsContainer}>
-          <div className={styles.buttons}>Team 1</div>
-          <div className={styles.buttons}>Team 1</div>
-          <div className={styles.buttons}>Team 1</div>
-          <div className={styles.buttons}>Team 1</div>
+          <div className={styles.buttons}>Design</div>
+          <div className={styles.buttons}>External</div>
+          <div className={styles.buttons}>Finance</div>
+          <div className={styles.buttons}>Marketing</div>
+          <div className={styles.buttons}>Operations</div>
+          <div className={styles.buttons}>Sponsorship</div>
+          <div className={styles.buttons}>Technical</div>
+        </div>
+
+        <div className={styles.team}>
+          {/* one profile */}
+          <div className={styles.profile}>
+            <img/>
+            <h4>First last</h4>
+            <p> Position, lead</p>
+          </div>
+
+          {/* 2nd profile */}
+          <div className={styles.profile}>
+            <img/>
+            <h4>First last</h4>
+            <p> Position, lead</p>
+          </div>
+
+          
         </div>
 
     </div>
