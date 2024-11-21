1c84da4960cb81b69db7e3c7df98391a036c6baa
michelleyeoh <michellew.yeoh@gmail.com>
2024-11-20 20:17:18 -0800

    finished hero desktop

--- app/(pages)/(about-us)/about-us/page.tsx
+++ app/(pages)/(about-us)/about-us/page.tsx
@@ -1,3 +1,12 @@
+import HeroStats from "../_components/hero/heroStats";
+import HeroText from "../_components/hero/heroText";
+
 export default function AboutUs() {
-  return <div>Halo this is the about us page!</div>;
+  return (
+    <div>
+      <div style={{margin:'500px'}}>Halo this is the about us page!</div>
+      <HeroText/>
+      <HeroStats/>
+    </div>
+  );
 }
---
+++ app/(pages)/_components/CountUp/CountUp.tsx
@@ -0,0 +1,77 @@
+// CountUp.tsx from 2024 website
+import React, { useEffect, useState, useRef } from 'react';
+
+interface CountUpProps {
+  end: number;
+  duration: number;
+  prefix?: string;
+  suffix?: string;
+}
+
+const CountUp: React.FC<CountUpProps> = ({
+  end,
+  duration,
+  prefix = '',
+  suffix = '',
+}) => {
+  const [count, setCount] = useState(0);
+  const [start, setStart] = useState(false);
+  const countUpRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const isCentered = (ref: React.RefObject<HTMLDivElement>) => {
+      const rect = ref.current?.getBoundingClientRect();
+      if (rect) {
+        const windowHeight = window.innerHeight;
+        return (
+          rect.top >= 0 &&
+          rect.bottom <= (windowHeight || document.documentElement.clientHeight)
+        );
+      }
+      return false;
+    };
+
+    const handleScroll = () => {
+      if (countUpRef.current && isCentered(countUpRef) && !start) {
+        setStart(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [start]);
+
+  useEffect(() => {
+    // if (start) {
+      let startTimestamp: number | null = null;
+      const step = (timestamp: number) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = timestamp - startTimestamp;
+        const currentCount = Math.min(end, (progress / duration) * end);
+        setCount(currentCount);
+        if (progress < duration) {
+          window.requestAnimationFrame(step);
+        }
+      };
+
+      window.requestAnimationFrame(step);
+    // }
+  }, [start, end, duration]);
+
+  return (
+    <div
+      ref={countUpRef}
+      style={{ fontFamily: 'Metropolis', color: '005271' }}
+    >
+      {/* {start && (
+        <> */}
+          {prefix}
+          {Math.floor(count)}
+          {suffix}
+        {/* </>
+      )} */}
+    </div>
+  );
+};
+
+export default CountUp;
