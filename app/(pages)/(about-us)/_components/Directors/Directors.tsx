59a4f797b6b6c83808983677242aa0747964bafc
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-11-24 16:25:46 -0800

    es linting

--- app/(pages)/(about-us)/_components/Directors/Directors.tsx
+++ app/(pages)/(about-us)/_components/Directors/Directors.tsx
@@ -1,58 +1,54 @@
 import styles from './Directors.module.scss';
-import { useState } from "react";
-const [activeCategory, setActiveCategory] = useState('Technical');
+// import { useState } from 'react';
+// const [activeCategory, setActiveCategory] = useState('Technical');
 
 export default function Directors() {
   return (
     <div className={styles.container}>
+      <div className={styles.text}>
+        <h1>Meet our Directors</h1>
+        <h3>HackDavis is built by students, for students.</h3>
+      </div>
 
-        <div className={styles.text}>
-          <h1>Meet our Directors</h1>
-          <h3>HackDavis is built by students, for students.</h3>
+      <div className={styles.buttonsContainer}>
+        <div className={styles.buttons}>Design</div>
+        <div className={styles.buttons}>External</div>
+        <div className={styles.buttons}>Finance</div>
+        <div className={styles.buttons}>Marketing</div>
+        <div className={styles.buttons}>Operations</div>
+        <div className={styles.buttons}>Sponsorship</div>
+        <div className={styles.buttons}>Technical</div>
+      </div>
+
+      <div className={styles.team}>
+        {/* one profile */}
+        <div className={styles.profile}>
+          {/* <Image /> */}
+          <h4>First last</h4>
+          <p> Position, lead</p>
         </div>
 
-        <div className={styles.buttonsContainer}>
-          <div className={styles.buttons}>Design</div>
-          <div className={styles.buttons}>External</div>
-          <div className={styles.buttons}>Finance</div>
-          <div className={styles.buttons}>Marketing</div>
-          <div className={styles.buttons}>Operations</div>
-          <div className={styles.buttons}>Sponsorship</div>
-          <div className={styles.buttons}>Technical</div>
+        {/* 2nd profile */}
+        <div className={styles.profile}>
+          {/* <Image /> */}
+          <h4>First last</h4>
+          <p> Position, lead</p>
         </div>
 
-        <div className={styles.team}>
-          {/* one profile */}
-          <div className={styles.profile}>
-            <img/>
-            <h4>First last</h4>
-            <p> Position, lead</p>
-          </div>
-
-          {/* 2nd profile */}
-          <div className={styles.profile}>
-            <img/>
-            <h4>First last</h4>
-            <p> Position, lead</p>
-          </div>
-
-          {/* 3rd profile */}
-          <div className={styles.profile}>
-            <img/>
-            <h4>First last</h4>
-            <p> Position, lead</p>
-          </div>
-
-          {/* 4th profile */}
-          <div className={styles.profile}>
-            <img/>
-            <h4>First last</h4>
-            <p> Position, lead</p>
-          </div>
-
-
+        {/* 3rd profile */}
+        <div className={styles.profile}>
+          {/* <Image /> */}
+          <h4>First last</h4>
+          <p> Position, lead</p>
         </div>
 
+        {/* 4th profile */}
+        <div className={styles.profile}>
+          {/* <Image /> */}
+          <h4>First last</h4>
+          <p> Position, lead</p>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        'background-light': "var(--background-light)",
-        'background-dark': "var(--background-dark)",
-        'foreground-light': "var(--foreground-light)",
-        'foreground-dark': "var(--foreground-dark)",
-        'highlight': "var(--highlight)",
-        'text-dark': "var(--text-dark)",
-        'text-light': "var(--text-light)"
+        'background-light': 'var(--background-light)',
+        'background-dark': 'var(--background-dark)',
+        'foreground-light': 'var(--foreground-light)',
+        'foreground-dark': 'var(--foreground-dark)',
+        highlight: 'var(--highlight)',
+        'text-dark': 'var(--text-dark)',
+        'text-light': 'var(--text-light)',
       },
       spacing: {
-        'edge': "var(--edge-spacer)"
+        edge: 'var(--edge-spacer)',
       },
       fontFamily: {
-        'metropolis': "var(--font-metropolis)",
-        'montserrat': "var(--font-montserrat)",
-        'inter': "var(--font-inter)",
-        'jakarta': "var(--font-jakarta)"
-      }
+        metropolis: 'var(--font-metropolis)',
+        montserrat: 'var(--font-montserrat)',
+        inter: 'var(--font-inter)',
+        jakarta: 'var(--font-jakarta)',
+      },
     },
   },
   plugins: [],
