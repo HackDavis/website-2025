24fc6bbb88a60711039bd3d992630dadf8f5fb64
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-12-05 13:27:07 -0800

    Created values & styling

--- app/(pages)/(about-us)/about-us/page.tsx
+++ app/(pages)/(about-us)/about-us/page.tsx
@@ -1,3 +1,9 @@
+import Values from '../_components/Values/Values';
 export default function AboutUs() {
-  return <div>Halo this is the about us page!</div>;
+  return (
+    <div>
+      Halo this is the about us page!
+      <Values />
+    </div>
+  );
 }
