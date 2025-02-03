727d0a4af2127b1c8d41010e835deadf30f4966d
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-03 11:20:56 -0800

    Added Seal and adjusted next.config.js

---
+++ app/(pages)/_components/Donate/Donate.tsx
@@ -0,0 +1,29 @@
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import style from './Donate.module.scss';
+// import Bunny from 'public/images/landing_page/BunnySnorkel.svg';
+// import Caustics from 'public/images/landing_page/Caustics.svg';
+// import WaterRays from 'public/images/landing_page/WaterRays.svg';
+
+const Underwater = () => (
+  <div className={style.container}>
+    <h1>support for social good</h1>
+    <Link
+      href="https://www.guidestar.org/profile/shared/4cf39094-2e34-44e6-890e-7a547408155b"
+      target="_blank"
+    >
+      <div className={style.sealWrapper}>
+        <Image
+          src="https://widgets.guidestar.org/TransparencySeal/10110290"
+          alt="Guidestar Transparency Seal"
+          fill={true}
+          unoptimized={true}
+        />
+      </div>
+    </Link>
+  </div>
+);
+
+export default Underwater;
