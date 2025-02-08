3e295e5efe05c6ff55deaf82fa9471c9f22d0025
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-02-08 13:27:09 -0800

    moved files around

--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -3,17 +3,15 @@ import SandCastle from './_components/SandCastle';
 import Sponsors from './_components/Sponsors/Sponsors';
 import styles from './page.module.scss';
 import FAQ from './_components/FAQ/FAQ';
-import Donate from '@app/(pages)/_components/Donate/Donate';
 
 export default function Home() {
   return (
     <div>
-      <Donate />
       <div className={styles.container} id="home">
-      <div className="h-[300px] w-full bg-red-500"></div>
-      <div className="flex flex-col items-center justify-center">
-        <SandCastle />
-      </div>
+        <div className="h-[300px] w-full bg-red-500"></div>
+        <div className="flex flex-col items-center justify-center">
+          <SandCastle />
+        </div>
       </div>
 
       {/* Sections for Testing */}
