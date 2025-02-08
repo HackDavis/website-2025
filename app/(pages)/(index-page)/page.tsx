61a6cf7d58803aa98f2fdbdc3c5b23bdc6cc21ba
Win Cheng <winzamark12@gmail.com>
2025-02-08 12:02:46 -0800

    Feat/sand castle (#30)
    * added prettier linting for formatting tailwind
    
    * adjusting background and assets
    
    * added all assets, adjusting positions
    
    * roughly done hehe XD
    
    ---------
    
    Co-authored-by: Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>

--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -1,4 +1,5 @@
 // import Directors from './(about-us)/_components/Directors/Directors';
+import SandCastle from './_components/SandCastle';
 import Sponsors from './_components/Sponsors/Sponsors';
 import styles from './page.module.scss';
 import FAQ from './_components/FAQ/FAQ';
@@ -7,23 +8,27 @@ export default function Home() {
   return (
     <div className={styles.container}>
       {/* Intro Section */}
-      <p className="text-4xl font-bold font-metropolis invisible">
+      {/* <p className="invisible font-metropolis text-4xl font-bold">
         Halo! Welcome to the HackDavis template repo :D
       </p>
-      <p className="text-4xl font-bold font-montserrat">
+      <p className="font-montserrat text-4xl font-bold">
         Halo! Welcome to the HackDavis template repo :D
       </p>
-      <p className="text-4xl font-bold font-inter">
+      <p className="font-inter text-4xl font-bold">
         Halo! Welcome to the HackDavis template repo :D
       </p>
-      <p className="text-4xl font-bold font-jakarta">
+      <p className="font-jakarta text-4xl font-bold">
         Halo! Welcome to the HackDavis template repo :D
-      </p>
+      </p> */}
+      <div className="h-[300px] w-full bg-red-500"></div>
+      <div className="flex flex-col items-center justify-center">
+        <SandCastle />
+      </div>
 
       {/* Sections for Testing */}
       <div
         id="about"
-        className="h-[800px] w-full flex items-center justify-center bg-gray-100"
+        className="flex h-[800px] w-full items-center justify-center bg-gray-100"
       >
         <h2 className="text-3xl font-bold">About Section</h2>
       </div>
