2672bfd2cd9024e53a4a53a4ae29236c0ef01ae8
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-08 09:17:39 -0800

    Adjusted styling for large desktop view

--- app/(pages)/_components/Donate/Donate.tsx
+++ app/(pages)/_components/Donate/Donate.tsx
@@ -13,21 +13,24 @@ const Donate = () => (
       </div>
 
       <div className={style.children}>
-        <div className={style.sealWrapper}>
-          <Link
-            className={style.sealChildren}
-            href="https://www.guidestar.org/profile/shared/4cf39094-2e34-44e6-890e-7a547408155b"
-            target="_blank"
-          >
-            <Image
-              src="https://widgets.guidestar.org/TransparencySeal/10110290"
-              alt="Guidestar Transparency Seal"
-              fill={true}
-              unoptimized={true}
-            />
-          </Link>
+        <div className={style.darkGreenContainer}>
+          <div className={style.sealChildren}>
+            <Link
+              href="https://www.guidestar.org/profile/shared/4cf39094-2e34-44e6-890e-7a547408155b"
+              target="_blank"
+            >
+              <div className={style.sealImageWrapper}>
+                <Image
+                  src="https://widgets.guidestar.org/TransparencySeal/10110290"
+                  alt="Guidestar Transparency Seal"
+                  fill={true}
+                  unoptimized={true}
+                />
+              </div>
+            </Link>
+          </div>
 
-          <div className={style.buttons}>
+          <div className={style.sealWrapper}>
             <a className={style.sponsorButton}>
               <div className={style.heartButton}>
                 <Image src={Heart} alt="Heart blue icon" unoptimized={true} />
