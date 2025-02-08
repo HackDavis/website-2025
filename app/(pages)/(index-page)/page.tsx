da0789ec9130944fa9fec6cf34bbcdfeb8ab1e8c
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-02-08 12:51:15 -0800

    removed placeholder code

--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -10,19 +10,6 @@ export default function Home() {
     <div>
       <Donate />
       <div className={styles.container} id="home">
-        {/* Intro Section */}
-        {/* <p className="invisible font-metropolis text-4xl font-bold">
-          Halo! Welcome to the HackDavis template repo :D
-        </p>
-        <p className="font-montserrat text-4xl font-bold">
-          Halo! Welcome to the HackDavis template repo :D
-        </p>
-        <p className="font-inter text-4xl font-bold">
-          Halo! Welcome to the HackDavis template repo :D
-        </p>
-        <p className="font-jakarta text-4xl font-bold">
-          Halo! Welcome to the HackDavis template repo :D
-        </p> */}
       <div className="h-[300px] w-full bg-red-500"></div>
       <div className="flex flex-col items-center justify-center">
         <SandCastle />
@@ -32,16 +19,12 @@ export default function Home() {
       {/* Sections for Testing */}
       <div
         id="about"
-        className="flex h-[800px] w-full items-center justify-center bg-gray-100"
+        className="flex h-[1000px] w-full items-center justify-center bg-gray-100"
       >
-        <h2 className="text-3xl font-bold">About Section</h2>
+        <h2 className="text-3xl font-bold">Underwater Section</h2>
       </div>
 
-      <div
-      // id="faq"
-      // className="h-[800px] w-full flex items-center justify-center bg-gray-200"
-      >
-        {/* <h2 className="text-3xl font-bold">FAQ Section</h2> */}
+      <div>
         <FAQ />
       </div>
 
