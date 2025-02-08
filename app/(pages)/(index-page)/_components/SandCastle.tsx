accc71843ce4a30ddff0c6d6ee24a177ec67b17e
Win Cheng <winzamark12@gmail.com>
2025-02-08 08:44:48 -0800

    roughly done hehe XD

--- app/(pages)/(index-page)/_components/SandCastle.tsx
+++ app/(pages)/(index-page)/_components/SandCastle.tsx
@@ -1,5 +1,4 @@
-import dark_sand_wave from '@public/images/what_is_hackdavis/dark_sand.svg';
-import medium_sand from '@public/images/what_is_hackdavis/medium_sand.svg';
+import sand_background from '@public/images/what_is_hackdavis/sand_background.svg';
 import froggy from '@public/images/what_is_hackdavis/Froggy.svg';
 import shell from '@public/images/what_is_hackdavis/shell.svg';
 import sandcastle from '@public/images/what_is_hackdavis/sandcastle.svg';
@@ -13,9 +12,8 @@ import Image from 'next/image';
 
 export default function SandCastle() {
   return (
-    <main>
+    <main className="overflow-hidden">
       <SandCastleBackground>
-        <SandCastleTexture />
         <SandCastleContent />
       </SandCastleBackground>
     </main>
@@ -24,8 +22,8 @@ export default function SandCastle() {
 
 function SandCastleTexture() {
   return (
-    <div className="absolute inset-0">
-      <div className="absolute left-[15%] top-[80%] border border-red-500">
+    <div className="absolute inset-0 h-[950px] w-full">
+      <div className="absolute left-[15%] top-[65%]">
         <div className="relative h-[80px] w-[80px]">
           <Image
             src={shell}
@@ -35,17 +33,17 @@ function SandCastleTexture() {
           />
         </div>
       </div>
-      <div className="absolute left-[35%] top-[10%] border border-red-500">
+      <div className="absolute left-[35%] top-[10%]">
         <div className="relative h-[50px] w-[50px]">
           <Image src={star} alt="star" fill style={{ objectFit: 'contain' }} />
         </div>
       </div>
-      <div className="absolute left-[60%] top-[110%] border border-red-500">
-        <div className="relative h-[50px] w-[50px]">
+      <div className="absolute left-[60%] top-[40%]">
+        <div className="relative h-[100px] w-[100px]">
           <Image src={crab} alt="crab" fill style={{ objectFit: 'contain' }} />
         </div>
       </div>
-      <div className="absolute left-[0] top-[95%] border border-red-500">
+      <div className="absolute left-[0] top-[65%]">
         <div className="relative h-[50px] w-[50px]">
           <Image
             src={sand_hole1}
@@ -55,7 +53,7 @@ function SandCastleTexture() {
           />
         </div>
       </div>
-      <div className="absolute left-[50%] top-[70%]">
+      <div className="absolute left-[50%] top-[35%]">
         <div className="relative h-[100px] w-[100px]">
           <Image
             src={sand_hole2}
@@ -81,13 +79,13 @@ function SandCastleTexture() {
 
 function SandCastleContent() {
   return (
-    <div className="flex flex-row flex-wrap">
-      <div className="w-[500px]">
+    <div className="flex h-fit w-full flex-row flex-wrap p-[10%]">
+      <div className="w-full sm:w-full md:w-[400px] lg:w-[500px]">
         <WhatIsHackDavis />
       </div>
-      <div className="w-1/2">
-        <div className="relative -mt-32 flex h-full w-full justify-end">
-          <div className="relative h-[250px] w-[250px]">
+      <div className="w-full md:w-1/3">
+        <div className="relative mt-8 flex h-full w-full justify-center md:-mt-10 md:justify-end">
+          <div className="relative h-[150px] w-[150px] sm:h-[200px] sm:w-[200px] md:h-[250px] md:w-[250px]">
             <Image
               src={sandcastle}
               alt="sandcastle"
@@ -95,7 +93,7 @@ function SandCastleContent() {
               style={{ objectFit: 'contain' }}
             />
           </div>
-          <div className="relative -ml-24 h-[250px] w-[250px]">
+          <div className="relative -ml-12 h-[150px] w-[150px] sm:-ml-16 sm:h-[200px] sm:w-[200px] md:-ml-24 md:h-[250px] md:w-[250px]">
             <Image
               src={froggy}
               alt="froggy"
@@ -111,34 +109,23 @@ function SandCastleContent() {
 
 function SandCastleBackground({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex min-h-screen w-screen flex-col justify-center bg-sand-background-light">
+    <div className="flex h-auto w-screen flex-col justify-center bg-sand-background-light">
       {/* Medium sand layer */}
-      <div className="absolute inset-0 w-full">
-        <div className="min-w-screen relative h-full w-full">
-          <Image
-            src={medium_sand}
-            alt="medium sand"
-            fill
-            style={{ objectFit: 'cover' }}
-          />
+      <div className="relative h-[950px] w-full">
+        <Image
+          src={sand_background}
+          alt="sand background"
+          fill
+          className="object-cover"
+          style={{
+            minWidth: '100%',
+            minHeight: '100%',
+          }}
+        />
+        <SandCastleTexture />
+        <div className="absolute inset-0 z-10 flex w-full items-center justify-center">
+          {children}
         </div>
-      </div>
-
-      {/* Dark sand wave layer */}
-      <div className="absolute inset-0 w-full">
-        <div className="relative top-[40%] h-full w-full">
-          <Image
-            src={dark_sand_wave}
-            alt="dark sand"
-            fill
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-      </div>
-
-      {/* Content */}
-      <div className="relative z-10 border border-black p-[10%]">
-        {children}
       </div>
     </div>
   );
@@ -146,9 +133,9 @@ function SandCastleBackground({ children }: { children: React.ReactNode }) {
 
 function WhatIsHackDavis() {
   return (
-    <div className="flex flex-col items-start justify-center border border-red-500 p-4 text-sand-text-color">
-      <h1 className="text-5xl font-bold">HackDavis</h1>
-      <p className="text-2xl">
+    <div className="flex flex-col items-start justify-center p-4 text-sand-text-color">
+      <h1 className="text-3xl font-bold sm:text-4xl md:text-5xl">HackDavis</h1>
+      <p className="max-w-[90%] text-lg sm:max-w-[80%] sm:text-xl md:max-w-[70%] md:text-2xl">
         is one of the
         <strong> largest collegiate hackathon </strong> in Califronia, where
         over 950 students, creators, and leaders come together to{' '}
