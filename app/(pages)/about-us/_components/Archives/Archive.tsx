ed66e6f45bfcf34d0f85bd7077bbf0759b586007
michelleyeoh <michellew.yeoh@gmail.com>
2025-02-09 13:49:19 -0800

    changed import image

---
+++ app/(pages)/about-us/_components/Archives/Archive.tsx
@@ -0,0 +1,27 @@
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import styles from './Archive.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface ArchiveProps {
+  packageSrc: StaticImport;
+  vinylSrc: StaticImport;
+  alt: string;
+  link: string;
+}
+
+export default function Archive({
+  packageSrc,
+  vinylSrc,
+  alt,
+  link,
+}: ArchiveProps) {
+  return (
+    <Link href={link} target="_blank" className={styles.container}>
+      <div className={styles.package_container}>
+        <Image src={packageSrc} alt={alt} className={styles.package_src} />
+        <Image src={vinylSrc} alt={alt} className={styles.vinyl_src} />
+      </div>
+    </Link>
+  );
+}
