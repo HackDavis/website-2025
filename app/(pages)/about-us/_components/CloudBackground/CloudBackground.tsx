2814149a3997ad3ff0fb8fad87546b2b893b3c9d
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-08 19:00:04 -0800

    Fixed smaller clouds styling

--- app/(pages)/about-us/_components/CloudBackground/CloudBackground.tsx
+++ app/(pages)/about-us/_components/CloudBackground/CloudBackground.tsx
@@ -4,22 +4,8 @@ import Image from 'next/image';
 export default function CloudBackground() {
   return (
     <div className={styles.container}>
-      <div className={styles.child}>
-        <Image
-          src="/images/cloud_background/LeftClouds.svg"
-          alt="clouds"
-          width={150}
-          height={150}
-        />
-      </div>
-      <div className={styles.child}>
-        <Image
-          src="/images/cloud_background/RightClouds.svg"
-          alt="clouds"
-          width={150}
-          height={150}
-        />
-      </div>
+      <div className={styles.child1}></div>
+      <div className={styles.child2}></div>
     </div>
   );
 }
