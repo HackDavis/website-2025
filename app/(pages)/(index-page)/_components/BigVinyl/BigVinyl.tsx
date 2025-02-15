6684a76c442f8eb889760a4f2244678443515a5d
Anjali (Jay) Jain <116294517+JayJ104@users.noreply.github.com>
2025-02-15 03:14:04 -0800

    23 big vinyl footer (#60)
    * Fix vinyl placement
    
    * Attempted vinyl fixes
    
    * Footer responsiveness mostly fixed
    
    * Vinyl fixed and switched to tailwind
    
    * Footer Update
    
    * Fix vinyl scaling
    
    ---------
    
    Co-authored-by: Daniel Kim <hyunkim03@gmail.com>
    Co-authored-by: reehals <sunny.reehal@gmail.com>

--- app/(pages)/(index-page)/_components/BigVinyl/BigVinyl.tsx
+++ app/(pages)/(index-page)/_components/BigVinyl/BigVinyl.tsx
@@ -1,112 +1,108 @@
 'use client';
-//import { useParallax } from 'react-scroll-parallax';
-import styles from './BigVinyl.module.scss';
 import Image from 'next/image';
 
 import cloudBottom from '@public/BigVinyl/CloudBottom.svg';
 import cloudBottomDim from '@public/BigVinyl/CloudBottomDim.svg';
 import cloudCenter from '@public/BigVinyl/CloudCenter.svg';
 import cloudTop from '@public/BigVinyl/CloudTop.svg';
-//import vinyl from '/public/BigVinyl/Vinyl.svg';
 import vinyl from '@public/BigVinyl/Vinyl_text_style_1.svg';
-//import vinyl from '/public/BigVinyl/Vinyl_text_style_2.svg';
 
 export default function BigVinyl() {
-  // const parallax = useParallax<HTMLDivElement>({
-  //   rotate: [0, 360],
-  // });
-
   return (
-    <div className={styles.container}>
+    <div className="relative flex w-full flex-col items-center justify-evenly overflow-hidden bg-sky-background">
       {/* First Row */}
-      <div className={styles.rowTop}>
-        <div className={styles.cloudsTopLeft}>
-          <div className={styles.cloudsTopLeft_topLayer}>
+      <div className="z-2 relative flex h-[65vw] w-full justify-between lg:h-[calc((328/1440)*100vw-40px)]">
+        {/* Clouds Top Left - Always visible */}
+        <div className="relative left-1/2 w-full -translate-x-[90%] lg:-translate-x-[125%]">
+          <div className="z-2 relative top-[4%] aspect-[1069/328] w-[calc(1069/1440*100vw)] md:top-10">
             <Image
               src={cloudTop}
               alt="Clouds Top Left Layer 1"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
-          <div className={styles.cloudsTopLeft_bottomLayer}>
+          <div className="relative -top-[calc(328/1440*100vw)] aspect-[1117/339] w-[calc(1117/1440*100vw)] opacity-90">
             <Image
               src={cloudTop}
               alt="Clouds Top Left Layer 2"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
         </div>
-        <div className={styles.cloudsTopRight}>
-          <div className={styles.cloudsTopRight_topLayer}>
+
+        {/* Clouds Top Right - Only visible on lg, xl, 2xl */}
+        <div className="relative left-1/2 hidden -translate-x-[70%] lg:block">
+          <div className="z-2 relative top-10 aspect-[1069/328] w-[calc(1069/1440*100vw)] scale-x-[-1] transform">
             <Image
               src={cloudTop}
               alt="Clouds Top Right Layer 1"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
-          <div className={styles.cloudsTopRight_bottomLayer}>
+          <div className="relative -top-[calc(328/1440*100vw)] right-12 aspect-[1117/339] w-[calc(1117/1440*100vw)] scale-x-[-1] transform opacity-90">
             <Image
               src={cloudTop}
               alt="Clouds Top Right Layer 2"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
         </div>
       </div>
 
       {/* Second Row */}
-      <div className={styles.rowMiddle}>
-        <div className={styles.cloudCenter}>
-          <Image src={cloudCenter} alt="center cloud" />
+      <div className="z-1 relative h-0 lg:left-[2%] lg:h-[calc(248/1440*100vw+100px)] lg:w-full lg:-translate-y-[calc(74/1440*100vw)]">
+        <div className="relative aspect-[842/248] w-0 lg:top-[80px] lg:w-[calc(842/1440*100vw)]">
+          <Image
+            src={cloudCenter}
+            alt="Center Cloud"
+            className="h-full w-full"
+          />
         </div>
       </div>
 
       {/* Third Row */}
-      <div className={styles.rowBottom}>
-        <div className={styles.cloudsBottomLeft}>
-          <div className={styles.cloudsBottomLeft_topLayer}>
+      <div className="relative flex h-[calc(238/1440*100vw)] w-full justify-between">
+        {/* Clouds Bottom Left - Only visible on lg, xl, 2xl */}
+        <div className="left-1/2 hidden w-[calc(842/(2*1440)*100vw)] -translate-x-[105%] flex-col items-start lg:flex">
+          <div className="relative top-10 z-10 aspect-[842/234] w-[calc(842/1440*100vw)]">
             <Image
               src={cloudBottom}
               alt="Clouds Bottom Left Layer 1"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
-          <div className={styles.cloudsBottomLeft_bottomLayer}>
+          <div className="z-1 relative -top-[calc(234/1440*100vw)] aspect-[842/234] w-[calc(842/1440*100vw)]">
             <Image
               src={cloudBottomDim}
               alt="Clouds Bottom Left Layer 2"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
         </div>
 
-        <div className={styles.cloudsBottomRight}>
-          <div className={styles.cloudsBottomRight_topLayer}>
+        {/* Clouds Bottom Right - Only visible on lg, xl, 2xl */}
+        <div className="relative left-1/2 hidden -translate-x-[35%] lg:block">
+          <div className="relative top-9 z-10 aspect-[842/234] w-[calc(842/1440*100vw)] scale-x-[-1] transform">
             <Image
               src={cloudBottom}
               alt="Clouds Bottom Right Layer 1"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
-          <div className={styles.cloudsBottomRight_bottomLayer}>
+          <div className="z-1 relative -top-[calc(234/1440*100vw)] aspect-[842/234] w-[calc(842/1440*100vw)] opacity-60">
             <Image
               src={cloudBottom}
               alt="Clouds Bottom Right Layer 2"
-              objectFit="fill"
+              className="h-full w-full"
             />
           </div>
         </div>
       </div>
 
-      {/* Vinyl Row */}
-      <div className={styles.vinylRow}>
-        <div className={styles.bigVinyl}>
-          <Image
-            src={vinyl}
-            //ref={parallax.ref}
-            alt="Big Vinyl Center"
-            objectFit="fill"
-          />
+      {/* Vinyl Row - Large screens center it, Small screens move it to right edge */}
+      <div className="relative flex w-full">
+        <div className="animate-rotateVinyl absolute left-1/2 left-[100%] aspect-square w-[150vw] -translate-x-[100%] lg:left-[50%] lg:w-[80vw] lg:w-[min(calc(940*100vw/1440))] lg:-translate-x-1/2 lg:-translate-y-[60%]">
+          <Image src={vinyl} alt="Big Vinyl Center" className="h-full w-full" />
         </div>
       </div>
     </div>
--- app/(pages)/_components/Footer/Footer.tsx
+++ app/(pages)/_components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 'use client';
-import styles from './Footer.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -9,9 +8,8 @@ import {
   faInstagram,
   faDiscord,
 } from '@fortawesome/free-brands-svg-icons';
-import Image from 'next/image';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import footerGrass from '/public/Footer/grass.svg';
+import Image from 'next/image';
 config.autoAddCss = false;
 
 const scrollToTop = () => {
@@ -20,87 +18,112 @@ const scrollToTop = () => {
 
 export default function Footer() {
   return (
-    <footer className={styles.container}>
-      <div className={styles.footer_grass}>
-        <Image src={footerGrass} alt="Footer grass" objectFit="fill" />
+    <footer className="relative z-10 flex w-full flex-col bg-grass-background-dark">
+      {/* Footer Background Image */}
+      <div className="z-11 overflow-y-show absolute h-0 lg:h-[calc(425/1440*100vw)] lg:h-full lg:w-full">
+        <Image
+          src="/Footer/grass.svg"
+          alt="Footer Grass"
+          layout="fill"
+          objectFit="cover"
+          className="translate-y-[clamp(-65px,-0.032143*100vw+4.428571px,-25px)] transform"
+        />
       </div>
-      <div className={styles.contentContainer}>
-        <div className={styles.hackdavisLogo}>
+      <div className="z-11 overflow-y-show absolute left-1/2 h-[calc(205/1440*100vw)] h-full w-full -translate-x-1/2 lg:h-0">
+        <Image
+          src="/Footer/grass_topOnly.svg"
+          alt="Footer Grass"
+          layout="fill"
+          objectFit="cover"
+          className="translate-y-[clamp(-65px,-0.032143*100vw+4.428571px,-15px)] transform"
+        />
+      </div>
+
+      {/* Content Container */}
+      <div className="z-13 relative flex min-h-[calc(425/1440*100vw)] w-full flex-col items-center justify-around gap-2 px-6 md:flex-row md:justify-evenly md:gap-0 md:px-0">
+        {/* HackDavis Logo */}
+        <div className="mt-7 flex flex-row items-center gap-6 md:mt-0">
           <Image
             src="/Footer/hdLogoWhite.svg"
-            alt="hackdavis Logo"
+            alt="HackDavis Logo"
+            className="h-[72px] w-[72px] md:h-[92.7px] md:w-[92.5px]"
             width={92.5}
             height={92.7}
-            className={styles.hdLogoWhite}
           />
           <Image
             src="/Footer/hdLogoMottoUpdated.svg"
-            alt="hackdavis name and motto"
+            alt="HackDavis name and motto"
+            className="h-[55px] w-[176px] md:h-[71.55px] md:w-[226px]"
             width={226}
             height={71.55}
-            className={styles.hdMottoWhite}
           />
         </div>
-        <div className={styles.socialContent}>
-          <div className={styles.brandIcons}>
-            <a className={styles.singleIcon} href="mailto:team@hackdavis.io">
+
+        {/* Social Icons */}
+        <div className="flex w-2/3 flex-col items-center gap-4 md:w-[27vw]">
+          <div className="flex w-full max-w-[420px] flex-row items-center justify-between pt-2">
+            <a
+              className="w-8 text-white transition hover:text-gray-300"
+              href="mailto:team@hackdavis.io"
+            >
               <FontAwesomeIcon icon={faEnvelope} />
             </a>
             <a
-              className={styles.mediumIcon}
+              className="w-10 text-white transition hover:text-gray-300"
               href="https://medium.com/@HackDavis"
               target="#"
               rel="noopener noreferrer"
-              aria-label="@HackDavis on Medium"
             >
               <FontAwesomeIcon icon={faMedium} />
             </a>
             <a
-              className={styles.fbIcon}
+              className="w-5 text-white transition hover:text-gray-300"
               href="https://www.facebook.com/HackDavis"
               target="#"
               rel="noopener noreferrer"
-              aria-label="HackDavis on Facebook"
             >
               <FontAwesomeIcon icon={faFacebookF} />
             </a>
             <a
-              className={styles.singleIcon}
+              className="w-8 text-white transition hover:text-gray-300"
               href="https://twitter.com/hack_davis"
               target="#"
               rel="noopener noreferrer"
-              aria-label="@hack_davis on Twitter"
             >
               <FontAwesomeIcon icon={faTwitter} />
             </a>
             <a
-              className={styles.singleIcon}
+              className="w-8 text-white transition hover:text-gray-300"
               href="https://www.instagram.com/hackdavis"
               target="#"
               rel="noopener noreferrer"
-              aria-label="@hackdavis on Instagram"
             >
               <FontAwesomeIcon icon={faInstagram} />
             </a>
             <a
-              className={styles.discordIcon}
+              className="w-10 text-white transition hover:text-gray-300"
               href="https://discord.gg/wc6QQEc"
               target="#"
               rel="noopener noreferrer"
-              aria-label="HackDavis Discord server"
             >
               <FontAwesomeIcon icon={faDiscord} />
             </a>
           </div>
-          <p className={styles.copyright}>
-            &copy; 2024 HackDavis • Made with ☕️ & 💛 in Davis
+          <p className="mt-3 pb-3 font-metropolis text-sm text-white">
+            &copy; 2025 HackDavis • Made with ☕️ & 💛 in Davis
           </p>
         </div>
-        <div className={styles.arrowToTop} onClick={scrollToTop}>
-          <p className={styles.backToTopText}>BACK TO TOP</p>
+
+        {/* Back to Top Arrow */}
+        <div
+          className="absolute right-6 top-40 flex cursor-pointer flex-row items-center gap-2 text-white transition-transform hover:scale-110 sm:top-10 md:top-11 lg:top-16 xl:top-20"
+          onClick={scrollToTop}
+        >
+          <p className="text-xs md:text-base">BACK TO TOP</p>
           <Image
             src="/Footer/arrowUp.svg"
-            alt="arrow up"
+            alt="Arrow Up"
+            className="h-6 w-6"
             width={24}
             height={24}
           />
