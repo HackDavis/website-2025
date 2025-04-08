dc59c4d6e7ea9fc187669804478b0d56df8b91cd
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-07 18:58:06 -0700

    Created heading

--- app/(pages)/(index-page)/_components/Crowdfund/Crowdfund.tsx
+++ app/(pages)/(index-page)/_components/Crowdfund/Crowdfund.tsx
@@ -1,6 +1,7 @@
 // import Image from 'next/image';
 import CrowdfundCarousel from './_components/CrowdfundCarousel/CrowdfundCarousel';
 import styles from './Crowdfund.module.scss';
+import { LuHeart } from 'react-icons/lu';
 
 const supporters = [
   'Alice Johnson',
@@ -13,6 +14,10 @@ const supporters = [
 export default function Crowdfund() {
   return (
     <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1>And a special thanks to our donors</h1>
+        <LuHeart className={styles.heartIcon} />
+      </div>
       <div className={styles.sponsors}>
         <CrowdfundCarousel supporters={supporters} />
       </div>
